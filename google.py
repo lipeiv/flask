afe654b4b3af374055fb70d75d18ff27f7936dc0
lipeiv <lipeiv@qq.com>
2020-05-23 01:20:35 +0800

    mod

--- google.py
+++ google.py
@@ -3,6 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("<google_url>")
 def index():
-	return requests.get("https://www.google.com").text
+	return requests.get("https://www.google.com"+google_url).text
