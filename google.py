5fef0310b8c0157a9395b1aaf2a9c6f250ad536f
李培培 <lipeiv@lipeipeideMBP.lan>
2021-01-12 23:34:11 +0800

    mod

--- google.py
+++ google.py
@@ -1,8 +1,16 @@
 from flask import Flask
-import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/phone/number")
 def index():
-	return requests.get("https://www.google.com").text
+	with open('upload.txt') as f:
+		return f
+
+
+@app.route("/phone/register", methods=['POST'])
+def register():
+	phone_number = request.files['the_file']
+	with open('upload.txt', 'w') as f:
+		f.write(phone_number)
+	return "注册成功!"
